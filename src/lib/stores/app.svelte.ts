6c20ae30538b47b3aee975e4fabc9538d680a879
Thomas Lee <djvoa12@gmail.com>
2025-06-16 17:04:32 +0200

    Update app store

--- src/lib/stores/app.svelte.ts
+++ src/lib/stores/app.svelte.ts
@@ -1,6 +1,11 @@
+import type { Dog } from '$lib/types';
+
 class Store {
-  public isAuthenticated = $state(false);
-  public isDarkTheme = $state(true);
+  public favoriteDogs = $state<Dog[]>([]);
+  public isAuthenticated = $state<boolean>(false);
+  public isDarkTheme = $state<boolean>(true);
+
+  public favoriteDogIds = $derived(new Set(this.favoriteDogs.map((d) => d.id)));
 }
 
 export const store = new Store();
