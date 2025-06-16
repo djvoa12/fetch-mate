88ba6463fafd08fc3309b63c7b64b19f4a869092
Thomas Lee <djvoa12@gmail.com>
2025-06-16 17:34:05 +0200

    Add dog API

---
+++ src/lib/api/dog.ts
@@ -0,0 +1,47 @@
+import { goto } from '$app/navigation';
+import { store } from '$lib/stores/app.svelte';
+import type { Dog, QueryResult } from '$lib/types';
+import axios, { type AxiosError } from 'axios';
+import { dogServiceClient } from './http-client';
+
+export const getAllBreeds = async (): Promise<string[]> => {
+  const { data } = await dogServiceClient.get('/breeds');
+  return data;
+};
+
+export const queryDogIds = async (queryParams: Record<string, any>): Promise<QueryResult> => {
+  const params = new URLSearchParams();
+
+  for (const key in queryParams) {
+    const value = queryParams[key];
+
+    if (Array.isArray(value)) {
+      value.forEach((v) => params.append(key, v));
+    } else if (value !== undefined && value !== null) {
+      params.append(key, value);
+    }
+  }
+
+  try {
+    const { data } = await dogServiceClient.get(`/search?${params.toString()}`);
+    return data;
+  } catch (e: unknown | AxiosError) {
+    if (axios.isAxiosError(e)) {
+      store.isAuthenticated = false;
+      goto('/');
+    } else {
+      console.error(e);
+    }
+    return { resultIds: [], total : 0 };
+  }
+};
+
+export const getDogs = async (ids: string[]): Promise<Dog[]> => {
+  const { data } = await dogServiceClient.post('', ids);
+  return data;
+};
+
+export const matchDog = async (ids: string[]): Promise<string> => {
+  const { data } = await dogServiceClient.post('/match', ids);
+  return data.match;
+};
---
+++ src/lib/api/index.ts
@@ -0,0 +1,3 @@
+export * from './dog';
+export * from './http-client';
+// export * from './location';
