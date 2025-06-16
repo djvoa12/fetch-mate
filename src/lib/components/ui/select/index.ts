575d4b7815d19a563d4de04bfb8a271c85736331
Thomas Lee <djvoa12@gmail.com>
2025-06-16 12:49:48 +0200

    Add .prettierrc

--- src/lib/components/ui/select/index.ts
+++ src/lib/components/ui/select/index.ts
@@ -1,16 +1,16 @@
-import { Select as SelectPrimitive } from "bits-ui";
+import { Select as SelectPrimitive } from 'bits-ui';
 
-import Group from "./select-group.svelte";
-import Label from "./select-label.svelte";
-import Item from "./select-item.svelte";
-import Content from "./select-content.svelte";
-import Trigger from "./select-trigger.svelte";
-import Separator from "./select-separator.svelte";
-import ScrollDownButton from "./select-scroll-down-button.svelte";
-import ScrollUpButton from "./select-scroll-up-button.svelte";
-import GroupHeading from "./select-group-heading.svelte";
+import Group from './select-group.svelte';
+import Label from './select-label.svelte';
+import Item from './select-item.svelte';
+import Content from './select-content.svelte';
+import Trigger from './select-trigger.svelte';
+import Separator from './select-separator.svelte';
+import ScrollDownButton from './select-scroll-down-button.svelte';
+import ScrollUpButton from './select-scroll-up-button.svelte';
+import GroupHeading from './select-group-heading.svelte';
 
-import SelectionPill from "$lib/components/Select/SelectionPill.svelte";
+import SelectionPill from '$lib/components/Select/SelectionPill.svelte';
 
 const Root = SelectPrimitive.Root;
 
@@ -36,5 +36,5 @@ export {
   ScrollDownButton as SelectScrollDownButton,
   ScrollUpButton as SelectScrollUpButton,
   GroupHeading as SelectGroupHeading,
-  SelectionPill,
+  SelectionPill
 };
